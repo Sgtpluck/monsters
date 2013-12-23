cdd4fcaa0e7306896c89f28463097eea30324910
Davida Marion <davidamarion@gmail.com>
2013-12-23 13:15:36 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MonsterParty::Application.config.secret_key_base = 'd832ad673d8694aacc367d9a71c3332d29cc458df03586f28a0e80795f0a8ad1abe64226908b76a07102d17d2736118a214170209b39b95d963a85aad24b44eb'
