d3d3a208d0c5be05155d8bd9b7ec4174e2a1a8d2
Davida Marion <davidamarion@gmail.com>
2014-01-13 21:38:59 -0800

    creates unique and repeat visitor log

--- app/models/visitor.rb
+++ app/models/visitor.rb
@@ -3,5 +3,18 @@ class Visitor < ActiveRecord::Base
   def add_visit
     self.update(visits: (visits+1))
   end
+
+  def self.ips
+    @ips = []
+    all.each do |visitor|
+      @ips << visitor.ip
+    end
+  end
+
+
+  def self.unique
+    ips
+    @ips.uniq.length
+  end
   
 end
